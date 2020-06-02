766f359e7cfeadba007f1ce1ce020f3c84d5c756
BerkayKirtilli <berkay.krtll.06@gmail.com>
2020-06-02 21:32:39 +0300

    Fractional added

---
+++ Fractional.java
@@ -0,0 +1,48 @@
+
+public class Fractional {
+	public final long numerator;
+	public final long denominator;
+	public static final String NotANumber = "Not a Number";
+	public static final String PositiveInfinity = "+Infinity";
+	public static final String NegativeInfinity = "-Infinity";
+	
+	Fractional(long numerator, long denominator) {	
+		// student code
+	}
+	
+	Fractional simplify() {
+		// student code
+	}
+	
+	// Not a Number (NaN) 
+	boolean isNaN() {
+		// student code
+	}
+
+	boolean isInfinity() {
+		// student code
+	}
+	
+	static int sign(long numerator, long denominator) {
+		// student code
+	}
+	
+	int sign() {
+		// student code
+	}
+
+	double getValue() {
+		return (double)numerator / denominator;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// student code
+	}
+	
+	@Override
+	public String toString() {
+		// student code
+	}
+
+}
